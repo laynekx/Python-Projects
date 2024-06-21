60212968acf143772b8edd2defecdfbef3ad5258
Pauline <pdevillacorta@gmail.com>
2024-06-21 12:02:53 +0800

    New files

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("DarkSlateGrey")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
